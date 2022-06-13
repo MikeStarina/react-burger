15b39ff4b6fbd46a0cff55b05db350792ea9a832
Mike Starina <x@pnhd.ru>
2022-06-13 22:53:50 +0300

    some fixes

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import './App.css';
+import styles from './app.module.css';
 
-import AppHeader from './components/app-header/app-header.jsx';
-import BurgerIngredients from './components/burger-ingredients/burger-ingredients.jsx';
-import BurgerConstructor from './components/burger-constructor/burger-constructor.jsx';
-import Modal from './components/modal/modal.jsx';
+import AppHeader from '../app-header/app-header.jsx';
+import BurgerIngredients from '../burger-ingredients/burger-ingredients.jsx';
+import BurgerConstructor from '../burger-constructor/burger-constructor.jsx';
+import Modal from '../modal/modal.jsx';
 
 
 
